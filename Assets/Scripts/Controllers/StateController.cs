1f4828f76ad08666bb884529533d56ef053d1dbd
WizMager <wizmager@gmail.com>
2022-04-21 16:33:12 +0300

    ref: change UI image(need rework) feat: ui enable when player can do something

--- Assets/Scripts/Controllers/StateController.cs
+++ Assets/Scripts/Controllers/StateController.cs
@@ -100,6 +100,7 @@ namespace Controllers
 
         private void RocketCrushed()
         {
+            _firstPersonView.gameObject.SetActive(false);
             OnStateChange?.Invoke(GameState.RocketCrushed);
             _deadView.gameObject.SetActive(true);
             Debug.Log(GameState.RocketCrushed);
@@ -108,7 +109,7 @@ namespace Controllers
         private void Restart()
         {
             OnStateChange?.Invoke(GameState.Restart);
-            _firstPersonView.gameObject.SetActive(false);
+            _firstPersonView.gameObject.SetActive(true);
             _finalScreenView.gameObject.SetActive(false);
             _deadView.gameObject.SetActive(false);
             _startPosition.SetRestart();
@@ -190,12 +191,14 @@ namespace Controllers
 
         private void EndToCenterGravity()
         {
+            _firstPersonView.gameObject.SetActive(true);
             OnStateChange?.Invoke(GameState.FlyAroundPlanet);
             Debug.Log(GameState.FlyAroundPlanet);
         }
 
         private void EndRotateAround()
         {
+            _firstPersonView.gameObject.SetActive(false);
             OnStateChange?.Invoke(GameState.EdgeGravityFromPlanet);
             Debug.Log(GameState.EdgeGravityFromPlanet);
         }
