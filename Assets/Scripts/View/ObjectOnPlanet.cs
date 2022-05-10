b4cf41a2cd4a98db373a2e6de9ef0fd52f247076
vasghausen <v.v.okonechnikov@gmail.com>
2022-05-10 19:37:21 +0300

    fix: explosion2

---
+++ Assets/Scripts/View/ObjectOnPlanet.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace View
+{
+
+    public class ObjectOnPlanet : MonoBehaviour
+    {
+        private Rigidbody _rb;
+        private bool _onTheGround = true;
+
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            if (!_onTheGround) return;
+            var groundRay = new Ray(transform.position, -transform.up);
+            var raycastHit = new RaycastHit[1];
+
+            if (Physics.RaycastNonAlloc(groundRay, raycastHit, 1f) >= 1)
+            {
+                return;
+            }
+
+            _onTheGround = false;
+            _rb.AddForce(transform.up * Random.Range(0f, 2f), ForceMode.Impulse);
+            _rb.angularVelocity = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f),
+                Random.Range(-1f, 1f));
+        }
+
+    }
+}
